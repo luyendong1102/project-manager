053123d4feb6ea8e59bc618f973ca55ebcf0d5c4
SteveDng <45103768+luyendong1102@users.noreply.github.com>
2021-12-05 16:24:52 +0700

    sorry wrong config

--- src/main/java/edu/mngprj/mgprj/MgprjApplication.java
+++ src/main/java/edu/mngprj/mgprj/MgprjApplication.java
@@ -1,8 +1,5 @@
 package edu.mngprj.mgprj;
 
-import edu.mngprj.mgprj.entities.User;
-import edu.mngprj.mgprj.entities.UserLogin;
-import edu.mngprj.mgprj.entities.UserRole;
 import edu.mngprj.mgprj.repositories.UserRepository;
 import edu.mngprj.mgprj.repositories.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.sql.Date;
-import java.util.HashSet;
 
 @SpringBootApplication
 public class MgprjApplication implements CommandLineRunner {
@@ -33,48 +27,48 @@ public class MgprjApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // create role
-        UserRole admin_test = new UserRole();
-        admin_test.setId(1L);
-        admin_test.setRole_name("ROLE_ADMIN_TESTER");
-        userRoleRepository.save(admin_test);
-        UserRole admin = new UserRole();
-
-        admin.setId(2L);
-        admin.setRole_name("ROLE_ADMIN");
-        userRoleRepository.save(admin);
-        UserRole quanly = new UserRole();
-
-        quanly.setId(3L);
-        quanly.setRole_name("ROLE_QUANLY");
-        userRoleRepository.save(quanly);
-        UserRole nhanvien = new UserRole();
-
-        nhanvien.setId(4L);
-        nhanvien.setRole_name("ROLE_NHANVIEN");
-        userRoleRepository.save(nhanvien);
-
-        // add default account
-        User user = new User();
-        user.setId(1L);
-        user.set_enable(true);
-        user.setName("ASS MINN");
-        user.setPhone("0123456789");
-        user.setResume("Verry bad at school");
-        user.setBirthdate(Date.valueOf("1999-03-01"));
-
-        user.setRoles(new HashSet<>());
-        user.getRoles().add(admin);
-        user.getRoles().add(admin_test);
-        user.getRoles().add(quanly);
-        user.getRoles().add(nhanvien);
-
-        UserLogin userLogin = new UserLogin();
-        userLogin.setUser(user);
-        userLogin.setUsername("admin");
-        userLogin.setPassword(passwordEncoder.encode("1"));
-
-        user.setUserLogin(userLogin);
-
-        userRepository.save(user);
+//        UserRole admin_test = new UserRole();
+//        admin_test.setId(1L);
+//        admin_test.setRole_name("ROLE_ADMIN_TESTER");
+//        userRoleRepository.save(admin_test);
+//        UserRole admin = new UserRole();
+//
+//        admin.setId(2L);
+//        admin.setRole_name("ROLE_ADMIN");
+//        userRoleRepository.save(admin);
+//        UserRole quanly = new UserRole();
+//
+//        quanly.setId(3L);
+//        quanly.setRole_name("ROLE_QUANLY");
+//        userRoleRepository.save(quanly);
+//        UserRole nhanvien = new UserRole();
+//
+//        nhanvien.setId(4L);
+//        nhanvien.setRole_name("ROLE_NHANVIEN");
+//        userRoleRepository.save(nhanvien);
+//
+//        // add default account
+//        User user = new User();
+//        user.setId(1L);
+//        user.set_enable(true);
+//        user.setName("ASS MINN");
+//        user.setPhone("0123456789");
+//        user.setResume("Verry bad at school");
+//        user.setBirthdate(Date.valueOf("1999-03-01"));
+//
+//        user.setRoles(new HashSet<>());
+//        user.getRoles().add(admin);
+//        user.getRoles().add(admin_test);
+//        user.getRoles().add(quanly);
+//        user.getRoles().add(nhanvien);
+//
+//        UserLogin userLogin = new UserLogin();
+//        userLogin.setUser(user);
+//        userLogin.setUsername("admin");
+//        userLogin.setPassword(passwordEncoder.encode("1"));
+//
+//        user.setUserLogin(userLogin);
+//
+//        userRepository.save(user);
     }
 }
