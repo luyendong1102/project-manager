fad16d3a30853fec86a6c12d1fb3577ae2239b8e
SteveDng <45103768+luyendong1102@users.noreply.github.com>
2021-12-06 17:13:08 +0700

    add config

--- src/main/java/edu/mngprj/mgprj/services/TaskDetailsSerivce.java
+++ src/main/java/edu/mngprj/mgprj/services/TaskDetailsSerivce.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TaskDetailsSerivce {
-
-    @Autowired
-    private TaskDetailsSerivce taskDetailsSerivce;
-
+    
     @Autowired
     private UserRepository userRepository;
 
